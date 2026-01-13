f531d614b186cad4faa519209c4e7fc8e1f90c45
Sana Bulbule <sanabulbule@mac.lan>
2026-01-13 00:05:24 -0600

    Leetcode problem - two sum

--- src/arrays/two_sum.ts
+++ src/arrays/two_sum.ts
@@ -1,8 +1,33 @@
-function query(arr: number[], i: number, j: number): number {
-    let answer = 0;
-    for (let k = i; k <= j; k++) {
-        answer = answer + arr[k];
-    }
-    return answer;
-}
+/**
 
+Problem 3: Two Sum (Medium)
+Time: 20 minutes
+
+Given an array of integers and a target sum, return the indices of two numbers that add up 
+to the target. You may assume exactly one solution exists, and you cannot use the same element twice.
+
+Example:
+Input: nums = [2, 7, 11, 15], target = 9
+Output: [0, 1]
+
+Explanation: nums[0] + nums[1] = 2 + 7 = 9
+
+Hint: Use a hash map for O(n) solution.
+
+ */
+
+export function twoSum(nums: number[], target: number): number[] {
+    const numMap = new Map<number, number>();
+
+    for (let i = 0; i < nums.length; i++) {
+        const complement = target - nums[i];
+        if (numMap.has(complement)) {
+            return [numMap.get(complement)!, i];
+        }
+        numMap.set(nums[i], i);
+    }
+
+    throw new Error('No two sum solution found.');
+};
+
+console.log(twoSum([2, 7, 11, 15], 9)); // Output: [0, 1]
